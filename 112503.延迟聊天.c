ddd0ed3727854b0656d1ac305eede3e101271588
chuwenqi04 <74294888+chuwenqi04@users.noreply.github.com>
2020-11-25 16:52:16 +0800

    Create 112503.延迟聊天.c

---
+++ 112503.延迟聊天.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{
+printf("娜娜：你愁啥!!!\n");
+Sleep(2000);
+{printf("七七：瞅你咋地!!!\n");}
+Sleep(2000);
+{printf("娜娜：没咋地，你继续!!!，大哥您忙。\n");}
+Sleep(2000);
+{printf("七七：小弟干得不错。跟我干!!!\n");}
+
+}
