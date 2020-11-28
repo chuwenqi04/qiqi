2076e0cee0bc6464a4e2163619becea90c87b90b
chuwenqi04 <74294888+chuwenqi04@users.noreply.github.com>
2020-11-28 20:22:22 +0800

    Create 112804.降序排序.c

---
+++ 112804.降序排序.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b&&b>c)
+	{
+	printf("%d\n%d\n%d\n",a,b,c);
+	}
+	else if(b>a&&a>c)
+	{
+	printf("%d\n%d\n%d\n",b,a,c);
+	}
+	else if(b>c&&c>a)
+	{
+	printf("%d\n%d\n%d\n",b,c,a);
+	}
+	else if(a>c&&c>b)
+	{
+	printf("%d\n%d\n%d\n",a,c,b);
+	}
+	else if(c>a&&a>b)
+	{
+	printf("%d\n%d\n%d\n",c,a,b);
+	}
+	else if(c>b&&b>a)
+	{
+	printf("%d\n%d\n%d\n",c,b,a);
+	}
+
+}
